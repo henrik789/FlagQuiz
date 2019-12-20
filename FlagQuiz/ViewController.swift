4c637b563f74161735b5beb9484952ef0542c865
Henrik789 <henrik789@github.com>
2019-12-20 17:58:06 +0100

    Initial Commit

---
+++ FlagQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlagQuiz
+//
+//  Created by Henrik on 2019-12-20.
+//  Copyright © 2019 Henrik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
