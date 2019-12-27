fecff197f2f86a4a71eea26264a221e8d3f539d0
Henrik789 <henrik789@github.com>
2019-12-27 17:52:30 +0100

    Images in folder instead

--- FlagQuiz/FlagViewController.swift
+++ FlagQuiz/FlagViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
-import Kingfisher
+
 
 class FlagViewController: UIViewController {
     
+    var pictures = [String]()
     var getFlags = GetFlags()
     @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var landOne: UIButton!
@@ -31,50 +32,68 @@ class FlagViewController: UIViewController {
     }
     @IBOutlet weak var flagImage: UIImageView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        downloadFlagImage()
+        let docPath = Bundle.main.resourcePath! + "/" + "Countries"
+        let fileManager = FileManager.default
+        
+        let filesFromBundle = try! fileManager.contentsOfDirectory(atPath: docPath)
+        print(filesFromBundle, docPath)
+        
+        //        let fm = FileManager.default
+        //        let path = Bundle.main.resourcePath!
+        //        let items = try! fm.contentsOfDirectory(atPath: path)
+        //
+        //        for item in items {
+        //            if item.hasPrefix("a"){
+        //           pictures.append(item)
+        //            }
+        //        }
+        //        print(pictures)
+        //        print(pictures.count)
         
     }
     
-    func downloadFlagImage() {
-        let land = getFlags.buildFlagArray()
-        let url = URL(string: "https://www.countryflags.io/\(land)/flat/64.png")
-        flagImage.kf.indicatorType = .activity
-        flagImage.kf.setImage(with: url)
-        setCountryName(land: land)
-    }
     
-    func setCountryName(land: String) {
-        let answer = getFlags.checkCountry(landCode: land)
-        let randomNumber = Int.random(in: 1...4)
-        if randomNumber == 1 {
-            landOne.setTitle(answer, for: .normal)
-            setOtherNames(correctAnswer: answer, fake1: landTwo, fake2: landThre, fake3: landFour)
-        } else if randomNumber == 2 {
-            landTwo.setTitle(answer, for: .normal)
-            setOtherNames(correctAnswer: answer, fake1: landOne, fake2: landThre, fake3: landFour)
-        } else if randomNumber == 3 {
-            landThre.setTitle(answer, for: .normal)
-            setOtherNames(correctAnswer: answer, fake1: landOne, fake2: landTwo, fake3: landFour)
-        } else if randomNumber == 4 {
-            landFour.setTitle(answer, for: .normal)
-            setOtherNames(correctAnswer: answer, fake1: landOne, fake2: landTwo, fake3: landThre)
-        }
-    }
     
-    func setOtherNames(correctAnswer: String, fake1: UIButton, fake2: UIButton, fake3: UIButton) {
-        
-        let land = getFlags.buildFlagArray()
-        let fakeAnswer = getFlags.checkCountry(landCode: land)
-        if fakeAnswer != correctAnswer {
-            fake1.setTitle(fakeAnswer, for: .normal)
-            fake2.setTitle(fakeAnswer, for:  .normal)
-            fake3.setTitle(fakeAnswer, for:  .normal)
-        }
-        
-    }
+    //    func downloadFlagImage() {
+    //        let land = getFlags.buildFlagArray()
+    //        let url = URL(string: "https://www.countryflags.io/\(land)/flat/64.png")
+    //        flagImage.kf.indicatorType = .activity
+    //        flagImage.kf.setImage(with: url)
+    //        setCountryName(land: land)
+    //    }
+    //
+    //    func setCountryName(land: String) {
+    //        let answer = getFlags.checkCountry(landCode: land)
+    //        let randomNumber = Int.random(in: 1...4)
+    //        if randomNumber == 1 {
+    //            landOne.setTitle(answer, for: .normal)
+    //            setOtherNames(correctAnswer: answer, fake1: landTwo, fake2: landThre, fake3: landFour)
+    //        } else if randomNumber == 2 {
+    //            landTwo.setTitle(answer, for: .normal)
+    //            setOtherNames(correctAnswer: answer, fake1: landOne, fake2: landThre, fake3: landFour)
+    //        } else if randomNumber == 3 {
+    //            landThre.setTitle(answer, for: .normal)
+    //            setOtherNames(correctAnswer: answer, fake1: landOne, fake2: landTwo, fake3: landFour)
+    //        } else if randomNumber == 4 {
+    //            landFour.setTitle(answer, for: .normal)
+    //            setOtherNames(correctAnswer: answer, fake1: landOne, fake2: landTwo, fake3: landThre)
+    //        }
+    //    }
+    //
+    //    func setOtherNames(correctAnswer: String, fake1: UIButton, fake2: UIButton, fake3: UIButton) {
+    //
+    //        let land = getFlags.buildFlagArray()
+    //        let fakeAnswer = getFlags.checkCountry(landCode: land)
+    //        if fakeAnswer != correctAnswer {
+    //            fake1.setTitle(fakeAnswer, for: .normal)
+    //            fake2.setTitle(fakeAnswer, for:  .normal)
+    //            fake3.setTitle(fakeAnswer, for:  .normal)
+    //        }
+    //
+    //    }
     
 }
--- FlagQuiz/GetFlags.swift
+++ FlagQuiz/GetFlags.swift
@@ -6,17 +6,16 @@ class GetFlags {
     
 
     var urlArray: [URL] = []
-    let flagArray = ["AD", "AE", "AF","AG", "AI", "AL", "AM", "AN", "AO", "AQ", "AR", "AS",
-                    "AT", "AU", "AW", "AX", "AZ", "BA"]
+    let flagArray = ["AD", "AF", "AG","AL", "AO", "BS"]
     
-    func getAllFlags(completion: @escaping ([URL]) -> Void){
-        for index in 0..<flagArray.count {
-            let land = flagArray[index]
-            let imageUrl = URL(string: "https://www.countryflags.io/\(land)/flat/64.png")
-            urlArray.append(imageUrl!)
-        }
-        completion(urlArray)
-    }
+//    func getAllFlags(completion: @escaping ([URL]) -> Void){
+//        for index in 0..<flagArray.count {
+//            let land = flagArray[index]
+//            let imageUrl = URL(string: "https://www.countryflags.io/\(land)/flat/64.png")
+//            urlArray.append(imageUrl!)
+//        }
+//        completion(urlArray)
+//    }
     
     func buildFlagArray() -> String {
         let randomNumber = Int.random(in: 0..<flagArray.count)
@@ -28,40 +27,36 @@ class GetFlags {
         switch landCode {
         case "AD":
             return "Andorra"
-        case "AE":
-            return "United Arab Emirates"
         case "AF":
             return "Afghanistan"
         case "AG":
             return "Antigua and Barbuda"
-        case "AI":
-            return "Anguilla"
         case "AL":
             return "Albania"
-        case "AM":
-            return "Armenia"
-        case "AN":
-            return "Netherlands Antilles"
         case "AO":
             return "Angola"
-        case "AQ":
-            return "Antarctica"
-        case "AR":
-            return "Argentina"
-        case "AS":
-            return "American Samoa"
-        case "AT":
-            return "Austria"
-        case "AU":
-            return "Australia"
-        case "AW":
-            return "Aruba"
-        case "AX":
-            return "Åland"
-        case "AZ":
-            return "Azerbadjan"
-        case "BA":
-            return "Bosnia and Herzegovina"
+        case "BS":
+            return "Bahamas"
+//        case "AO":
+//            return "Angola"
+//        case "AQ":
+//            return "Antarctica"
+//        case "AR":
+//            return "Argentina"
+//        case "AS":
+//            return "American Samoa"
+//        case "AT":
+//            return "Austria"
+//        case "AU":
+//            return "Australia"
+//        case "AW":
+//            return "Aruba"
+//        case "AX":
+//            return "Åland"
+//        case "AZ":
+//            return "Azerbadjan"
+//        case "BA":
+//            return "Bosnia and Herzegovina"
 
         default:
             return "Sweden"
